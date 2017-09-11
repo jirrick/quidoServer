c896ecfa815d5b33b69dfd37eaa2e66389e4de5c
jirrick <jurahudec@gmail.com>
2017-09-11 11:00:07 +0200

    first version

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+const express = require('express'),
+    helmet = require('helmet'),
+    app = express(),
+    port = process.env.PORT || 3001;
+
+function rndOut() {
+    var text = "";
+    var possible = "01x";
+    
+    for (var i = 0; i < 8; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    
+    return text;
+    }
+
+app.use(helmet());
+
+app.get('/listen', function (req, res) {
+    console.log(req.originalUrl);
+    var outs = rndOut();
+    res.set('Content-Type', 'text/xml');
+    res.send('<?xml version="1.0" encoding="ISO-8859-1"?><root><set outs="' + outs + '"/></root>');
+})
+
+
+app.listen(port);
+
+console.log('quido server started on: ' + port);
+
