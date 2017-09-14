b3346750dc7dd3b8096eca7dee7e52f361488553
jirrick <jurahudec@gmail.com>
2017-09-14 17:11:33 +0200

    set ENV=PROD for Docker, set ENV=DEV otherwise

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ RUN npm install --production
 # Show current folder structure in logs
 #RUN ls -al -R
 
-CMD [ "pm2-docker", "start", "pm2.json" ]
+CMD [ "pm2-docker", "start", "pm2.json", "--env" ,"production" ]
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const express = require('express'),
     helmet = require('helmet'),
     app = express(),
+    environment = process.env.NODE_ENV || 'development',
     port = process.env.PORT || 3001;
 
 function rndOut() {
@@ -17,7 +18,7 @@ function rndOut() {
 app.use(helmet());
 
 app.get('/', function (req, res) {
-    res.send('Welcome to jirrick\'s quidoServer!');
+    res.send('Welcome to jirrick\'s quidoServer @ ' + environment);
 })
 
 
