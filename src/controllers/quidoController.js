34bf313799a71ab1f89501391429bf526f16d8f1
jirrick <jurahudec@gmail.com>
2017-09-15 01:26:18 +0200

    handle boards differently

--- src/controllers/quidoController.js
+++ src/controllers/quidoController.js
@@ -4,6 +4,35 @@ const mongoose = require('mongoose'),
     Logs = mongoose.model('Logs'),
     environment = process.env.NODE_ENV || 'development';
 
+//Index page
+exports.main = function (req, res) {
+    res.send('Welcome to jirrick\'s quidoServer @ ' + environment);
+};
+
+//Listen page
+exports.listen = function (req, res) {
+    //console.log(req.originalUrl);
+
+    //do different stuff for different board
+    if (req.query.name === 'LEVY')
+        levy(req, res);
+    else if (req.query.name === 'PRAVY')
+        pravy(req, res);
+    else 
+        res.status(400).send('Invalid request!');
+};
+
+//View page
+exports.viewAll = function (req, res) {
+    Logs.find({}, function (err, task) {
+        if (err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+//Private methods ----------------------------------------
+//Generate random output
 function rndOut() {
     let text = '';
     const possible = '01x';
@@ -14,13 +43,8 @@ function rndOut() {
     return text;
 }
 
-exports.main = function (req, res) {
-    res.send('Welcome to jirrick\'s quidoServer @ ' + environment);
-};
-
-exports.listen = function (req, res) {
-    //console.log(req.originalUrl);
-
+//Handle 'LEVY' board
+function levy(req, res){
     //Parse data from request into object
     const parsed = new Object();
     parsed.name = req.query.name;
@@ -36,15 +60,15 @@ exports.listen = function (req, res) {
         console.log(data);
     });
 
-    const outs = 'xxxxxxxx'; //rndOut();
+    //Send reply - do nothing
+    res.set('Content-Type', 'text/xml');
+    res.send('<?xml version="1.0" encoding="ISO-8859-1"?><root><set outs="xxxxxxxx"/></root>');
+}
+
+//Handle 'PRAVY' board
+function pravy(req, res){
+    //Send reply - do random shit
+    const outs = rndOut();
     res.set('Content-Type', 'text/xml');
     res.send('<?xml version="1.0" encoding="ISO-8859-1"?><root><set outs="' + outs + '"/></root>');
-};
-
-exports.viewAll = function (req, res) {
-    Logs.find({}, function (err, task) {
-        if (err)
-            res.send(err);
-        res.json(task);
-    });
-};
+}
