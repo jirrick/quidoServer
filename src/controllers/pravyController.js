29d099bb2d3bcbb23f1c8e2035a94adaf45b86c8
jirrick <jurahudec@gmail.com>
2017-09-16 21:34:21 +0200

    use boardInfo for output

--- src/controllers/pravyController.js
+++ src/controllers/pravyController.js
@@ -3,17 +3,17 @@
 //Handle 'PRAVY' board
 exports.process = function (boardInfo, req, res) {
     //Send reply - do random shit
-    const outs = rndOut();
+    const outs = rndOut(boardInfo.outputs);
     res.set('Content-Type', 'text/xml');
     res.send('<?xml version="1.0" encoding="ISO-8859-1"?><root><set outs="' + outs + '"/></root>');
 };
 
 //Generate random output
-function rndOut() {
+function rndOut(n) {
     let text = '';
     const possible = '01x';
 
-    for (var i = 0; i < 8; i++)
+    for (var i = 0; i < n; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
