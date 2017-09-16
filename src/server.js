0690924843f2db44463e88bfb6a9028c2577569b
jirrick <jurahudec@gmail.com>
2017-09-16 23:31:55 +0200

    use error handler

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express'),
+    errorHandler = require('strong-error-handler'),
     pretty = require('express-prettify'),
     helmet = require('helmet'),
     mongoose = require('mongoose'),
@@ -7,15 +8,20 @@ const express = require('express'),
     routes = require('./routes/quidoRoutes'),
     models = require('./models/quidoModel'),
     app = express(),
+    environment = process.env.NODE_ENV || 'development',
     port = process.env.PORT || 3001;
 
 //Set up default mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect(config.mongoDB, {useMongoClient: true});
+mongoose.connect(config.mongoDB, { useMongoClient: true });
 
-//Set up Helmet & Prettify
+//Set up middleware
 app.use(helmet());
 app.use(pretty({ query: 'pretty' }));
+app.use(errorHandler({
+    debug: environment === 'development',
+    log: true,
+}));
 
 //Set up routes and start app
 routes(app);
