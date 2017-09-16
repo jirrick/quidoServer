5b917b6804e7315ddd185b50c638fffc9a755127
jirrick <jurahudec@gmail.com>
2017-09-16 19:55:08 +0200

    reset counters when over treshold

--- src/controllers/levyController.js
+++ src/controllers/levyController.js
@@ -6,15 +6,19 @@ const mongoose = require('mongoose'),
 //Handle 'LEVY' board
 exports.process = function (boardInfo, req, res) {
     //Parse data from request into object
+    let resetCounters = false;
+    const tmpCounters = [];
+    for (let i = 1; i <= boardInfo.inputs; i++) {
+        const key = `cnt${i}`;
+        const value = parseInt(req.query[key]);
+        tmpCounters.push(value);
+        //Reset counters if one value is over treshold
+        if (value > 10000) resetCounters = true;
+    }
     const parsed = new Object();
     parsed.name = req.query.name;
     parsed.temp = req.query.tempV;
     parsed.timestamp = Date.now();
-    const tmpCounters = [];
-    for (let i = 1; i <= boardInfo.inputs; i++) {
-        const key = `cnt${i}`;
-        tmpCounters.push(parseInt(req.query[key]));
-    }
     parsed.counters = tmpCounters;
 
     //Create Log item and save to mongo
@@ -25,7 +29,22 @@ exports.process = function (boardInfo, req, res) {
         console.log(data);
     });
 
-    //Send reply - do nothing
+    //Send reply - do nothing or reset counters
+    let command = 'outs="xxxxxxxx"';
+    if (resetCounters)
+        command += resetString(tmpCounters);
+    console.log(command);
+    let reply = `<?xml version="1.0" encoding="ISO-8859-1"?><root><set ${command}/></root>`;
     res.set('Content-Type', 'text/xml');
-    res.send('<?xml version="1.0" encoding="ISO-8859-1"?><root><set outs="xxxxxxxx"/></root>');
+    res.send(reply);
 };
+
+//Generate reset counters string
+function resetString(arr) {
+    let text = '';
+    for (var i = 0, len = arr.length; i < len; i++) {
+        text += ` cnt${i+1}="${arr[i]}"`;
+    }
+
+    return text;
+}
