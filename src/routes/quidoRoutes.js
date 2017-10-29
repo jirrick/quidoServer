5242d7d26bab22e52d393378f0f954bba61db685
jirrick <jurahudec@gmail.com>
2017-10-29 17:03:03 +0100

    CRUD - boards

--- src/routes/quidoRoutes.js
+++ src/routes/quidoRoutes.js
@@ -6,24 +6,39 @@ const appController = require('../controllers/appController'),
     outputController = require('../controllers/outputController');
 
 module.exports = function (app) {
+    //MAIN
     app.route('/')
         .get(appController.main);
 
+    //PARSING
     app.route('/listen')
         .get(boardController.parse);
 
+    //OUTPUT VALUES
     app.route('/output/:name')
         .get(outputController.getValue);
 
     app.route('/output/:name/:value')
         .get(outputController.setValue);
 
+    //BROWSE DATA
     app.route('/view/:limit?')
         .get(viewController.jsonAll);
 
     app.route('/temps/:limit?')
         .get(viewController.jsonTemp);
 
+    //CONFIG - general
     app.route('/config')
         .get(appController.jsonConfigItems);
+
+    //CONFIG - boards
+    app.route('/config/boards')
+        .get(appController.jsonGetBoards)
+        .post(appController.jsonPostBoard);
+
+    app.route('/config/boards/:id')
+        .get(appController.jsonGetBoard)
+        .put(appController.jsonPutBoard)
+        .delete(appController.jsonDeleteBoard);
 };
